7eb1bb30621dd9e5c73cc4fb678b4af32d5f03b6
Alexis Johnson <alexis.johnson052@gmail.com>
2015-12-22 10:32:18 -0600

    fix back button

--- script.js
+++ script.js
@@ -5,17 +5,26 @@ $(document).ready(function(){
   $("#getDoge").click(function(){
     $(".welcome").slideUp();
     $("body").css("background", "black");
-    $("#previous").css("display", "inline")
-    $("#getDoge").html("Another!");
+    $("#getDoge").css("display", "none");
+    $("#previous").css("display", "inline");
+    $("#another").css("display", "inline");
     $.getJSON("https://api.unsplash.com/photos/search?client_id=28ab92b0a76bc92b6e43807c2e5456aca925f50b4148ec19afa5f64261fa7821&query=dog").done(function(response){
       var imageUrl = response[imageIndex].urls.regular;
       $("#doge").empty().append($("<img src=" + imageUrl + ">"))
+    }).fail(function(){
+      console.log("failed request")
+    })
+  });
+
+  $("#another").click(function(){
+    $.getJSON("https://api.unsplash.com/photos/search?client_id=28ab92b0a76bc92b6e43807c2e5456aca925f50b4148ec19afa5f64261fa7821&query=dog").done(function(response){
       if(imageIndex < response.length - 1){
         imageIndex += 1;
-      }
-      else{
+      }else{
         imageIndex = 0;
       }
+      var imageUrl = response[imageIndex].urls.regular;
+      $("#doge").empty().append($("<img src=" + imageUrl + ">"));
     }).fail(function(){
       console.log("failed request")
     })
@@ -23,16 +32,14 @@ $(document).ready(function(){
 
   $("#previous").click(function(){
     $.getJSON("https://api.unsplash.com/photos/search?client_id=28ab92b0a76bc92b6e43807c2e5456aca925f50b4148ec19afa5f64261fa7821&query=dog").done(function(response){
-      if(imageIndex>=2){
-        imageIndex -= 2;
+      if(imageIndex>0){
+        imageIndex -= 1;
         var imageUrl = response[imageIndex].urls.regular;
         $("#doge").empty().append($("<img src=" + imageUrl + ">"))
-        if(imageIndex < response.length - 1){
-          imageIndex += 1;
-        }
-        else{
-          imageIndex = 0;
-        }
+      }else{
+        imageIndex = response.length - 1;
+        var imageUrl = response[imageIndex].urls.regular;
+        $("#doge").empty().append($("<img src=" + imageUrl + ">"))
       }
     }).fail(function(){
       console.log("failed request")
