ffb6dfbb4a80f8cd4d9f32cac0cc1e011b9baea7
Alexis Johnson <alexis.johnson052@gmail.com>
2015-12-22 12:28:14 -0600

    style buttons

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
   var imageIndex = 0;
-  var doges = []
-  
+  var doges = [];
+
   var getDoges = function(){
     $.getJSON("https://api.unsplash.com/photos/search?client_id=28ab92b0a76bc92b6e43807c2e5456aca925f50b4148ec19afa5f64261fa7821&query=dog").done(function(response){
       doges = response;
