dcdae83039b6e1228a600492ee9fc018ea4315e6
Alexis Johnson <alexis.johnson052@gmail.com>
2015-12-28 16:25:34 -0600

    populate sudoku board

--- script.sudoku.js
+++ script.sudoku.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   $("#getSudoku").click(function(){
-    $("body").append("<table id='board'></table>")
-    makeBoard()
-    makeArray()
+    $("body").append("<table id='board'></table>");
+    makeBoard();
+    var array = makeArray();
+    assignValues(array);
   })
 
 
@@ -22,19 +23,45 @@ $(document).ready(function(){
     })
   }
 
-  var makeArray = function(){
-    var array = []
-    for(var i=0; i<9; i++){
-      array[i] = []
-      var j=0
-      while(j<9){
-        var num = Math.ceil(Math.random()*9)
-        if(array[i].indexOf(num) === -1){
-          array[i][j] = num
-          j++
-        }
+  var makeRow = function(){
+    var row = []
+    var j=0
+    while(j<9){
+      var num = Math.ceil(Math.random()*9)
+      if(row.indexOf(num) === -1){
+        row[j] = num
+        j++
       }
     }
-    console.log(array)
+    return row
+  }
+
+  var makeArray = function(){
+    var array = []
+    addRows:
+      while(array.length<9){
+        var row = makeRow()
+        for(var i=0; i<array.length; i++){
+          for(var j=0; j<9; j++){
+            if(array[i][j] === row[j]){
+              continue addRows;
+            }
+          }
+        }
+        array.push(row)
+      }
+    return array;
+  }
+
+  var assignValues = function(data){
+    for(i=0; i<9; i++){
+      d3.select(".r" + i)
+        .selectAll("td")
+        .data(data[i])
+        .text(function(d){
+          return d;
+        })
+    }
+
   }
 })
